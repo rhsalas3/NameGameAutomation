f407e55c8f427fb24aefd8071d300127e7f10ab5
rudys <r.salas@arca.com>
2018-01-01 20:17:33 -0500

    WillowTree Software Test Engineer Test Project - Rudy Salas
    Added an up-to-date version of the chromedriver to be used with my linux
    machine.
    
    Added validate*() methods to HomePage for the following test scenarios:
    <ul>
    <li>Verify that the "streak" counter is incrementing on correct selections.</li>
    <li>Verify that a  multiple “streak” counter resets after getting an incorrect answer.</li>
    <li>Verify that after 10 random selections the correct counters are being incremented for tries and correct counters.</li>
    <li>Verify name and displayed photos change after selecting the correct answer.</li>
    </ul>
    
    Added calls to each validate*() method within the WebTest class.
    
    Added "helper" methods which represent elements of the page which are
    commonly used, and methods for retrieving the names of the people in the
    gallery and the WebElement represented by a given name.

--- src/main/java/com/willowtreeapps/HomePage.java
+++ src/main/java/com/willowtreeapps/HomePage.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created on 5/23/17.
  */
@@ -23,17 +27,195 @@ public class HomePage extends BasePage {
 
     public void validateClickingFirstPhotoIncreasesTriesCounter() {
         //Wait for page to load
-        sleep(6000);
+        waitForPageToLoad();
 
         int count = Integer.parseInt(driver.findElement(By.className("attempts")).getText());
 
         driver.findElement(By.className("photo")).click();
 
-        sleep(6000);
+        waitForPageToLoad();
 
         int countAfter = Integer.parseInt(driver.findElement(By.className("attempts")).getText());
 
         Assert.assertTrue(countAfter > count);
 
     }
+
+    ///////////////////////////////
+    // ADDED BY RUDY SALAS
+    ///////////////////////////////
+
+    // This is done by retrieving the 'gallery' of photos, matching the name value of each photo in order to find the
+    // correct option, then clicking that option.
+    public void validateClickingCorrectPhotoIncreasesStreakCounter() {
+        waitForPageToLoad();
+
+        int count = getCurrentStreakCount();
+
+        // Find the photo that is correct and click it.
+        findPhoto(nameToBeFound().getText()).click();
+
+        waitForPageToLoad();
+
+        int countAfter = getCurrentStreakCount();
+
+        Assert.assertTrue(countAfter > count);
+    }
+
+    // This is done by selecting the correct photo for 3 consecutive attempts, which should set a streak. On the 4th
+    // attempt we will click a photo that is not the correct one. Then we'll retrieve the streak count and assert that
+    // it is now reset to 0.
+    public void validateStreakCounterResetsAfterIncorrectSelection() {
+        final int resetValue     = 0;
+        final int expectedStreak = 3;
+
+        waitForPageToLoad();
+
+        // Retrieve the current, starting steak count.
+        int startingStreak = getCurrentStreakCount();
+
+        // Process 3 consecutive attempts, selecting the correct photo so that a streak is started.
+        for (int i = 1; i <= expectedStreak; i++) {
+            // Find the photo that is correct and click it.
+            findPhoto(nameToBeFound().getText()).click();
+
+            waitForPageToLoad();
+
+            // Verify that the streak counter increments.
+            int countAfter = getCurrentStreakCount();
+            Assert.assertTrue(countAfter > startingStreak);
+        }
+
+        // Verify that the streak is 3 more than the starting value.
+        Assert.assertEquals(startingStreak + expectedStreak, getCurrentStreakCount());
+
+        // Now select a name that is not the correct option. To do this we will gather the list of names in the gallery
+        // and remove the correct option. Then we'll click the first photo that is not the correct one.
+        List<String> allIncorrectOptions = retrieveGallery();
+        allIncorrectOptions.remove(nameToBeFound().getText());
+        findPhoto(allIncorrectOptions.get(0)).click();
+
+        // Verify that the streak counter is now reset.
+        Assert.assertEquals(resetValue, getCurrentStreakCount());
+    }
+
+    // Verify that after 10 random selections the correct counters are being incremented for tries and correct counters
+    public void validateTriesAndCorrectSelectionCounters() {
+        waitForPageToLoad();
+
+        // Set up the running counter of the number of tries and correct selections.
+        int attempts                 = 10;
+        int startingNumOfTries       = Integer.parseInt(triesCounter().getText());
+        int startingNumCorrect       = Integer.parseInt(correctCounter().getText());
+        int numCorrectDuringAttempts = 0;
+
+        // Keep up with the gallery of selections.
+        List<String> remainingPhotosToSelect = retrieveGallery();
+
+        // Attempt 10 tries.
+        for (int i = 1; i <= attempts; i++) {
+            // Generate a random number between 0 and the number of remaining photos available to select.
+            int randomSelection = new Random().nextInt(remainingPhotosToSelect.size());
+
+            // If the random selection is that of the correct photo, increment the correct number counter.
+            if (remainingPhotosToSelect.get(randomSelection).equals(nameToBeFound().getText())) {
+                numCorrectDuringAttempts++;
+
+                // Click the photo.
+                findPhoto(remainingPhotosToSelect.get(randomSelection)).click();
+
+                // Allow time for the page to load then retrieve the new gallery.
+                waitForPageToLoad();
+                remainingPhotosToSelect = retrieveGallery();
+
+            } else {
+                // Otherwise, click the random selection and continue through the number of attempts.
+                findPhoto(remainingPhotosToSelect.get(randomSelection)).click();
+                remainingPhotosToSelect.remove(randomSelection); // Remove the already selected photo from the running list.
+            }
+
+        }
+
+        // Verify that the tries counter incremented correctly.
+        final int expectedTotalNumOfTries = startingNumOfTries + attempts;
+        Assert.assertEquals(expectedTotalNumOfTries, Integer.parseInt(triesCounter().getText()));
+
+        // Verify that the 'X correct' counter incremented correctly.
+        final int expectedTotalNumOfCorrectSelections = startingNumCorrect + numCorrectDuringAttempts;
+        Assert.assertEquals(expectedTotalNumOfCorrectSelections, Integer.parseInt(correctCounter().getText()));
+    }
+
+    public void validateNameAndGalleryChangesAfterCorrectGuess() {
+        waitForPageToLoad();
+
+        // Store the starting name to be guessed.
+        final String firstNameToGuess = nameToBeFound().getText();
+
+        // Retrieve all of the names in the gallery.
+        final List<String> firstGallery = retrieveGallery();
+
+        // Find and click on the correct photo. Verify that the web app responds as expected.
+        final int expectedStreak = getCurrentStreakCount() + 1;
+        findPhoto(nameToBeFound().getText()).click();
+        Assert.assertEquals(expectedStreak, getCurrentStreakCount());
+
+        waitForPageToLoad();
+
+        // Verify that the name to be guessed has changed to a new name.
+        Assert.assertFalse("Expected a new name to appear.", firstNameToGuess.equals(nameToBeFound().getText()));
+
+        // Verify that the gallery of photos has changed.
+        Assert.assertNotEquals("Expected the gallery of photos to change.", firstGallery, retrieveGallery());
+    }
+
+    // The name of the person to be selected.
+    private WebElement nameToBeFound() {
+        return driver.findElement(By.id("name"));
+    }
+
+    // The streak counter.
+    private WebElement streakCounter() {
+        return driver.findElement(By.className("streak"));
+    }
+
+    // The total number of tries counter.
+    private WebElement triesCounter() {
+        return driver.findElement(By.className("attempts"));
+    }
+
+    // The total number correct counter.
+    private WebElement correctCounter() {
+        return driver.findElement(By.className("correct"));
+    }
+
+    // Allows time for the page to appear/load. If after the wait time the name to be guessed is not present, the method
+    // will fail on an assertion.
+    private void waitForPageToLoad() {
+        sleep(6000);
+
+        Assert.assertTrue(nameToBeFound().isDisplayed());
+    }
+
+    // Find the photo within the gallery that matches the given name.
+    private WebElement findPhoto(final String name) {
+        final String pathToElement = String.format("//div[text()='%s']/..", name);
+
+        return driver.findElement(By.xpath(pathToElement));
+    }
+
+    private List<String> retrieveGallery() {
+        final List<String> allNames = new ArrayList<>();
+        final List<WebElement> gallery = driver.findElements(By.className("name"));
+
+        for (WebElement name : gallery) {
+            allNames.add(name.getText());
+        }
+
+        return allNames;
+    }
+
+    private int getCurrentStreakCount() {
+        return Integer.parseInt(streakCounter().getText());
+    }
+
 }
--- src/test/java/com/willowtreeapps/WebTest.java
+++ src/test/java/com/willowtreeapps/WebTest.java
@@ -19,7 +19,7 @@ public class WebTest {
      */
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.mac");
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
         Capabilities capabilities = DesiredCapabilities.chrome();
         driver = new ChromeDriver(capabilities);
         driver.navigate().to("http://www.ericrochester.com/name-game/");
@@ -35,6 +35,34 @@ public class WebTest {
     public void test_clicking_photo_increases_tries_counter() {
         new HomePage(driver)
                 .validateClickingFirstPhotoIncreasesTriesCounter();
+    }
+
+    // Verify that the "streak" counter is incrementing on correct selections.
+    @Test
+    public void test_correct_selection_increases_streak_counter() {
+        new HomePage(driver)
+                .validateClickingCorrectPhotoIncreasesStreakCounter();
+    }
+
+    // Verify that a  multiple “streak” counter resets after getting an incorrect answer.
+    @Test
+    public void test_streak_counter_resets_after_incorrect_selection() {
+        new HomePage(driver)
+                .validateStreakCounterResetsAfterIncorrectSelection();
+    }
+
+    // Verify that after 10 random selections the correct counters are being incremented for tries and correct counters.
+    @Test
+    public void test_tries_and_correct_counters_increment_correctly() {
+        new HomePage(driver)
+                .validateTriesAndCorrectSelectionCounters();
+    }
+
+    // Verify name and displayed photos change after selecting the correct answer.
+    @Test
+    public void test_name_and_gallery_changes_after_selecting_correct_answer() {
+        new HomePage(driver)
+                .validateNameAndGalleryChangesAfterCorrectGuess();
     }
 
     @After
